e9a6f2791fceb686ad7b7c367ba7986c71393b65
Ayaan-WHJ <78539077+Ayaan-WHJ@users.noreply.github.com>
2021-04-16 17:00:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 Webcam.set({
     width:350,
-    height:300,
+    height:350,
     image_format:'png',
     png_quality:90
 });
@@ -15,7 +15,7 @@ function take_snapshot(){
     } );
 }
 console.log('ml5 version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/D6mmON3bR/model.json',modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/SuKTIrq7Z/model.json',modelLoaded);
 
 function modelLoaded(){
     console.log('Model Loaded!');
@@ -25,7 +25,64 @@ function speak()
 {
 var synth = window.speechSynthesis;
 speak_data_1 = " The first prediction is " + prediction_1;
-speak_data_2 = " The second prediciton is " + prediction_2;
+speak_data_2 = " The second prediction is " + prediction_2;
 var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
-synth.speak(utterThis)
+synth.speak(utterThis);
 }
+
+
+function check()
+{
+img=document.getElementById('captured_image');
+classifier.classify(img,gotResult);
+}
+function gotResult(error,results)
+{
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("result_emotion_name").innerHTML = results[0].label;
+        document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+        speak();
+        if(results[0].label  == "victory")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+        if( results[0].label == "best")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if( results[0].label == "yo")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#129304;";
+
+        }
+        if( results[0].label == "loser")
+        {
+            document.getElementById("update_emoji").innerHTML = "&#128078;";
+
+        }
+        if(results[1].label =="victory")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#9996;";
+        }
+        if(results[1].label == "best")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128077;";
+        }
+        if(results[1].label == "yo")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#129304;";
+        }
+        if( results[0].label == "loser")
+        {
+            document.getElementById("update_emoji2").innerHTML = "&#128078;";
+
+        }
+    }
+}
+
